3da0eb02c5eb5d6aaa71cc79022cf9c055634db9
Michael Hitchens <mdhitchens@gmail.com>
2020-02-10 22:06:36 -0500

    Tweak house/spawn tele templates

--- tooltip.js
+++ tooltip.js
@@ -25,7 +25,7 @@ function respawnTeleTemplate(loc) {
     return div(
         p(imgWiki('b9/Respawn_Teleport.png'), aWiki('Respawn Teleport')),
         reqArceuus(),
-        p(text('Requires '), a('https://oldschool.runescape.wiki/w/Spawning#Respawn_point', 'respawn point'), text(' in ' + loc)),
+        p(text('Requires '), a('https://oldschool.runescape.wiki/w/Spawning#Respawn_point', 'respawn point in ' + loc)),
         reqMagic(34),
         p(runeReq('law', 1), runeReq('soul', 1))
     );
@@ -34,7 +34,7 @@ function respawnTeleTemplate(loc) {
 function teleToHouseTemplate(loc) {
     return div(
         p(imgWiki('88/Teleport_to_House.png'), aWiki('Teleport to House')),
-        p(text('Requires a house in '), a('https://oldschool.runescape.wiki/w/Player-owned_house', loc)),
+        p(text('Requires a '), a('https://oldschool.runescape.wiki/w/Player-owned_house', 'house in ' + loc)),
         reqMagic(40),
         p(runeReq('air', 1), runeReq('earth', 1), runeReq('law', 1))
     );
